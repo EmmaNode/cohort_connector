45f1567dff9c6df703df9d9188f6b50fb7df94e1
munimuni5”git config --global user.name munimuni5” <emileeleamons@gmail.com>
2018-11-09 22:42:56 -0800

    updating post, profile, and users. Status: fully working

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,1 +1,12 @@
 // fetch or create user profile, location, bio, experience, education, social net links
+
+// @route   GET api/profile/test
+// @desc    Tests profile route
+// @access  Public
+
+const express = require("express");
+const router = express.Router();
+
+router.get("/test", (req, res) => res.json({ msg: "Profile works" }));
+
+module.exports = router;
--- routes/api/users.js
+++ routes/api/users.js
@@ -1,3 +1,11 @@
 // users routes authentication login, passport user name email passowrd
-const express = require('express');
-consta router = express.Router();
+const express = require("express");
+const router = express.Router();
+
+// @route   GET api/users/test
+// @desc    Tests users route
+// @access  Public
+
+router.get("/test", (req, res) => res.json({ msg: "Users works" }));
+
+module.exports = router;
