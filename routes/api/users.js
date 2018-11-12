b7d891bc393828f9099847e50e592a4170225cec
munimuni5”git config --global user.name munimuni5” <emileeleamons@gmail.com>
2018-11-12 13:54:59 -0800

    Created POST route for user, login functionality and pasword hashing with bcrypt

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,11 +1,77 @@
 // users routes authentication login, passport user name email passowrd
 const express = require("express");
 const router = express.Router();
+const gravatar = require("gravatar");
+const bcrypt = require("bcryptjs");
+
+//Load user model
+const User = require("../../models/User");
 
 // @route   GET api/users/test
 // @desc    Tests users route
 // @access  Public
 
 router.get("/test", (req, res) => res.json({ msg: "Users works" }));
+
+// @route   GET api/users/register
+// @desc    Register user
+// @access  Public
+
+router.post("/register", (req, res) => {
+  User.findOne({ email: req.body.email }).then(user => {
+    if (user) {
+      return res.status(400).json({ email: "email already exists" });
+    } else {
+      const avatar = gravatar.url(req.body.email, {
+        s: "200", // size
+        r: "pg", // rating
+        d: "mm" // default
+      });
+      const newUser = new User({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password,
+        avatar
+      });
+
+      bcrypt.genSalt(10, (err, salt) => {
+        bcrypt.hash(newUser.password, salt, (err, hash) => {
+          if (err) throw err;
+          newUser.password = hash;
+          newUser
+            .save()
+            .then(user => res.json(user))
+            .catch(err => console.log(err));
+        });
+      });
+    }
+  });
+});
+
+// @route   GET api/users/login
+// @desc    Login user / Returning JWT Token
+// @access  Public
+
+router.post("/loging", (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+
+  // Find user by email
+  User.findOne({ email }).then(user => {
+    // check for user
+    if (!user) {
+      return res.status(404).json({ email: "User not found" });
+    }
+
+    // Check password
+    bcrypt.compare(password, user.password).then(isMatch => {
+      if (isMatch) {
+        res.json({ msg: "Success" });
+      } else {
+        return res.status(400).json({ password: "password incorrect" });
+      }
+    });
+  });
+});
 
 module.exports = router;
