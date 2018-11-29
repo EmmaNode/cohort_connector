be66e626e8706b8b96ffcc68949481a8817d3b4a
munimuni5”git config --global user.name munimuni5” <emileeleamons@gmail.com>
2018-11-28 19:31:21 -0800

    Passport JWT Authentication Strategy added

--- routes/api/users.js
+++ routes/api/users.js
@@ -5,6 +5,7 @@ const gravatar = require('gravatar');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const keys = require('../../config/keys');
+const passport = require('passport');
 
 //Load user model
 const User = require('../../models/User');
@@ -91,5 +92,21 @@ router.post('/login', (req, res) => {
     });
   });
 });
+
+// @route   GET api/users/login
+// @desc    Return current user
+// @access  Private
+router.get(
+  '/current',
+  passport.authenticate('jwt', { session: false }),
+  //the user lives inside the 'req' now, once token, auth and pass success
+  (req, res) => {
+    res.json({
+      id: req.user.id,
+      name: req.user.name,
+      email: req.user.email
+    });
+  }
+);
 
 module.exports = router;
