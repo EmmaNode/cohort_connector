a336f273e554f2c6873ca2bdda97fda3c2327ade
munimuni5”git config --global user.name munimuni5” <emileeleamons@gmail.com>
2018-11-28 19:45:46 -0800

    updated isEmpty function to an arrow function

--- validation/is-empty.js
+++ validation/is-empty.js
@@ -1,8 +1,5 @@
-function isEmpty(value) {
-  return (
-    value === undefined ||
-    value === null ||
-    (typeof value === 'object' && Object.keys(value).lenth === 0) ||
-    (typeof value === 'string' && value.trim().length === 0)
-  );
-}
+const isEmpty = value =>
+  value === undefined ||
+  value === null ||
+  (typeof value === 'object' && Object.keys(value).lenth === 0) ||
+  (typeof value === 'string' && value.trim().length === 0);
